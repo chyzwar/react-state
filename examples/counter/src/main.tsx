de5b636c0b4083d7192d02f5b15f0ccf9ef10c85
chyzwar <schizek.marcin@gmail.com>
2018-10-01 20:54:01 +0100

    added counter example

---
+++ examples/counter/src/main.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Provider from "@hyper/react-state/lib/Provider";
+import Subscribe from "@hyper/react-state/lib/Subscribe";
+import Container from "@hyper/react-state/lib/Container";
+
+type CounterState = {
+  count: number,
+};
+
+class CounterContainer extends Container<CounterState> {
+  private state = { count: 0 };
+
+  private increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  private decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+}
+
+function Counter() {
+  return (
+    <Subscribe to={[CounterContainer]}>
+      {(counter) => (
+        <div>
+          <button onClick={() => counter.decrement()}>-</button>
+          <span>{counter.state.count}</span>
+          <button onClick={() => counter.increment()}>+</button>
+        </div>
+      )}
+    </Subscribe>
+  );
+}
+
+ReactDOM.render(
+  <Provider>
+    <Counter />
+  </Provider>,
+  document.getElementById("root"),
+);
