67e2ec0e91bd1332cc32f27eedccf934ea55de59
chyzwar <schizek.marcin@gmail.com>
2018-12-24 20:21:53 +0000

    remove lib and sit forlder

--- lib/SubscribeProps.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Constructor from "@hyper/generic-types/lib/Constructor";
-import Container from "./Container";
-interface SubscribeProps<Containers extends Array<Constructor<Container>>> {
-    to: Containers;
-    children(...instances: Container[]): React.ReactNode;
-}
-export default SubscribeProps;
-//# sourceMappingURL=SubscribeProps.d.ts.map
